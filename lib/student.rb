cec391f7f8ad9271ef7fab5ce8f7fa16eb729498
Juan Tenezaca <tenj34@gmail.com>
2018-09-23 19:24:23 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -81,7 +81,9 @@ class Student
     WHERE grade = 10
     LIMIT 1
     SQL
-
+    first_student = DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first 
   end
 
   def save
