018134e01642d28c980dd8bbab94235b6d341a36
Juan Tenezaca <tenj34@gmail.com>
2018-09-23 19:11:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,8 +54,11 @@ class Student
     SELECT *
     FROM students
     WHERE grade BETWEEN 0 AND 12
-
     SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def save
